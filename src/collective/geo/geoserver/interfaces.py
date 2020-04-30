32ceab21bf5ab5338616d1fec41392e4981e9ed8
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2020-04-30 10:09:50 -0400

    Updated i18n support

--- src/collective/geo/geoserver/interfaces.py
+++ src/collective/geo/geoserver/interfaces.py
@@ -1,6 +1,8 @@
 from zope import schema
 from zope.interface import Interface
 
+from collective.geo.geoserver import geoMessageFactory as _
+
 
 class IGeoQuery(Interface):
     """ GeoQuery Content interface """
@@ -10,31 +12,31 @@ class IGeoServer(Interface):
     """ Geoserver Utility Interface """
 
     host = schema.TextLine(
-        title=u"Server",
-        description=u"An ip or name",
+        title=_(u"Server"),
+        description=_(u"An ip or name"),
         default=u"string:localhost",
         required=True)
 
     port = schema.TextLine(
-        title=u"Port",
-        description=u"Server Port",
+        title=_(u"Port"),
+        description=_(u"Server Port"),
         default=u"string:9001",
         required=True)
 
     url_wms = schema.TextLine(
-        title=u"WMS Path",
-        description=u"The wms path, domain excluded",
+        title=_(u"WMS Path"),
+        description=_(u"The wms path, domain excluded"),
         default=u"string:/geoserver/it.geosolutions/wms",
         required=True)
 
     rest_path = schema.TextLine(
-        title=u"REST Path",
-        description=u"Rest path for queries",
+        title=_(u"REST Path"),
+        description=_(u"Rest path for queries"),
         default=u"string:/geoserver/rest",
         required=True)
 
     cformat = schema.TextLine(
-        title=u"Format",
-        description=u"Format of results ",
+        title=_(u"Format"),
+        description=_(u"Format of results "),
         default=u"string:application%2Fvnd.google-earth.kml%2Bxml",
         required=True)
--- src/collective/geo/geoserver/locales/update.sh
+++ src/collective/geo/geoserver/locales/update.sh
@@ -3,3 +3,22 @@ domain=collective.geo.geoserver
 i18ndude rebuild-pot --pot $domain.pot --create $domain ../
 i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
 
+i18ndude rebuild-pot --pot ../locales/plone.pot --create plone  ../profiles
+i18ndude sync --pot ../locales/plone.pot ../locales/*/LC_MESSAGES/plone.po
+
+WARNINGS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
+ERRORS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
+FATAL=`find . -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
+
+echo
+echo "There are $WARNINGS warnings \(possibly missing i18n markup\)"
+echo "There are $ERRORS errors \(almost definitely missing i18n markup\)"
+echo "There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)"
+echo "For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or" 
+echo "Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir" 
+
+rm ./rebuild_i18n.log
+touch ./rebuild_i18n.log
+
+find ../ -name "*pt" | xargs i18ndude find-untranslated > ./rebuild_i18n.log
+# Ok, now poedit is your friend!
